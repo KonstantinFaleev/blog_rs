a66934deacabb55b14ed069fe3f3aab34a808f78
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-06-06 22:25:19 +0300

    Add contact model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_05_214816) do
+ActiveRecord::Schema.define(version: 2018_06_06_191427) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "email"
+    t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
